8ac06c4051e79973dc7a18c08e1d3ae4a617e170
Lucas Rocha <lucas.lgr.01@gmail.com>
2025-11-26 03:01:43 -0300

    adiciona biblioteca de icons lucide

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,4 +1,5 @@
 import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
+import { CircleCheck } from "lucide-react-native";
 
 import { styles } from "./styles";
 import { FilterStatus } from "@/types/FilterStatus";
@@ -11,6 +12,9 @@ type Props = TouchableOpacityProps & {
 export function Filter({ status, isActive, ...rest }: Props) {
     return (
         <TouchableOpacity style={[styles.container, {opacity: isActive ? 1 : 0.5}]} activeOpacity={0.8} {...rest}>
+            
+            <CircleCheck size={18}/>
+
             <Text style={styles.title}>
                 {status === FilterStatus.DONE ? "Comprados" : "Pendentes"}
             </Text>
