e353bb23c3892a3bea7298eca458e701d9b0457e
Lucas Rocha <lucas.lgr.01@gmail.com>
2025-11-26 02:36:17 -0300

    adiciona content

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,15 +1,19 @@
-import { View, Image } from "react-native"
-import { styles } from "./styles"
-import { Button } from "@/components/Button"
-import { Input } from "@/components/Input"
+import { View, Image } from "react-native";
+import { styles } from "./styles";
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-      <Input placeholder="O que você precisa comprar?" />
-      <Button title="Entrar"  />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}></View>
     </View>
-  )
+  );
 }
