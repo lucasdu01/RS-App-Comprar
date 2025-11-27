528abbe770e1aa4cf61f95cb5cf6db401a8fc449
Lucas Rocha <lucas.lgr.01@gmail.com>
2025-11-27 01:03:07 -0300

    adiciona statusIcon dinamicamente

--- src/components/Filter/styles.ts
+++ src/components/Filter/styles.ts
@@ -4,7 +4,7 @@ export const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
         alignItems: "center",
-        gap: 8,
+        gap: 5,
     },
     title: {
         fontSize: 12,
