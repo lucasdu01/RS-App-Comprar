72f28c9b90892438d48757231dc32e93864576aa
Lucas Rocha <lucas.lgr.01@gmail.com>
2025-11-28 01:18:24 -0300

    adiciona itemsByStatus na tela

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -27,12 +27,12 @@ export function Home() {
 			status: FilterStatus.PENDING,
 		}
 		await itemsStorage.add(newItem)
-		await getItems()
+		await itemsByStatus()
 	}
 
-	async function getItems(){
+	async function itemsByStatus(){
 		try {
-			const response = await itemsStorage.get()
+			const response = await itemsStorage.getByStatus(filter)
 			setItems(response)
 		} catch (error) {
 			console.log(error)
@@ -41,8 +41,8 @@ export function Home() {
 	}
 
 	useEffect(() => {
-		getItems()
-	}, [])
+		itemsByStatus()
+	}, [filter])
 
 	return (
 		<View style={styles.container}>
