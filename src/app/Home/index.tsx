dea402643a896b2d3dec817ee3f341b9f711fcff
Lucas Rocha <lucas.lgr.01@gmail.com>
2025-11-27 23:57:58 -0300

    adiciona funcao de adicionar Item

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Image, TouchableOpacity, Text, FlatList } from "react-native";
+import { View, Image, TouchableOpacity, Text, FlatList, Alert } from "react-native";
 
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
@@ -9,56 +9,69 @@ import { Item } from "@/components/Item";
 import { styles } from "./styles";
 import { FilterStatus } from "@/types/FilterStatus";
 
-const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE ];
-const ITEMS = [
-  { id: "1", status: FilterStatus.DONE, description: "1 pacote de café" },
-  { id: "2", status: FilterStatus.PENDING, description: "3 pacoteas de macarrao" },
-  { id: "3", status: FilterStatus.PENDING, description: "3 cebolas" },
-]
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
 export function Home() {
-  const [filter, setFilter] = useState(FilterStatus.PENDING);
+	const [filter, setFilter] = useState(FilterStatus.PENDING);
+	const [description, setDescription] = useState("");
+	const [items, setItems] = useState<any>([]);
 
-  return (
-    <View style={styles.container}>
-      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+	function handleAdd() {
+		if (!description.trim()) {
+			return Alert.alert("Adicionar", "Informe a descrição para adicionar.");
+		}
+		const newItem = {
+			id: Math.random().toString(36).substring(2),
+			description,
+			status: FilterStatus.PENDING,
+		};
+		setItems((prevState) => [...prevState, newItem]);
+	}
+	return (
+    	<View style={styles.container}>
+      		<Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-      <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
-        <Button title="Entrar" />
-      </View>
+      		<View style={styles.form}>
+        		<Input
+          			placeholder="O que você precisa comprar?"
+          			onChangeText={setDescription}
+				/>
+        		<Button title="Adicionar" onPress={handleAdd} />
+      		</View>
 
-      <View style={styles.content}>
-        <View style={styles.header}>
-          {FILTER_STATUS.map((status) => (
-              <Filter 
-                key={status}
-                status={status} 
-                isActive={status === filter}
-                onPress={() => setFilter(status)}
-              />
-            ))}
-          <TouchableOpacity style={styles.clearButton}>
-            <Text style={styles.clearText}>Limpar</Text>
-          </TouchableOpacity>
-        </View>
-        
-        <FlatList
-          data={ITEMS}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <Item
-              data={item}
-              onStatus={() => console.log("muda o status")}
-              onRemove={() => console.log("remove o item")}
-          />
-          )}
-          showsVerticalScrollIndicator={true}
-          ItemSeparatorComponent={() => <View style={styles.separator} />}
-          contentContainerStyle={styles.listContent}
-          ListEmptyComponent={() => <Text style={styles.empty}>Nenhum item aqui.</Text>}
-        />
-	    </View>
-    </View>
-  )
+      		<View style={styles.content}>
+				<View style={styles.header}>
+					{FILTER_STATUS.map((status) => (
+						<Filter
+							key={status}
+							status={status}
+							isActive={status === filter}
+							onPress={() => setFilter(status)}
+						/>
+					))}
+					<TouchableOpacity style={styles.clearButton}>
+						<Text style={styles.clearText}>Limpar</Text>
+					</TouchableOpacity>
+				</View>
+
+        		<FlatList
+					data={items}
+					keyExtractor={(item) => item.id}
+					renderItem={({ item }) => (
+						<Item
+							data={item}
+							onStatus={() => console.log("muda o status")}
+							onRemove={() => console.log("remove o item")}
+						/>
+					)}
+          			showsVerticalScrollIndicator={true}
+          			ItemSeparatorComponent={() => <View style={styles.separator} />}
+          			contentContainerStyle={styles.listContent}
+          			ListEmptyComponent={() => (
+            			<Text style={styles.empty}>Nenhum item aqui.</Text>
+          			)}
+				/>
+      		</View>
+    	</View>
+  );
 }
